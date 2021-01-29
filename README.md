7e4c7097f298b34cb371ff3b0f2f6413593a6faa
rvolo <rvolo@users.noreply.github.com>
2021-01-28 17:30:30 -0800

    cleanup for v1

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 XML-Replace-Action
 
-Simple github action to replace values in a xml file using xpath
+Simple github action to replace a value in a xml config file
+
+Sample replace action
+```yaml
+- name: Set logback env values
+  uses: rvolo/xml-replace-action@v0.2
+  with:
+    filepath: "config-file.xml"
+    xpath: "//configuration/server/port/text()"
+    replace: "5000"
+```
+
--- index.js
+++ index.js
@@ -7,18 +7,38 @@ const fs = require('fs');
 try {
     let filePath = core.getInput('filepath', {required: true});
     let xpathString = core.getInput('xpath', {required: true});
-    let replaceString = core.getInput('replace', {required: true});
+    let replaceString = core.getInput('replace');
 
+    writeFile(filePath, xpathString, replaceString);
+} catch (error) {
+    console.log(error)
+    core.setFailed(error.message);
+}
+
+function writeFile(filePath, xpathString, replaceString) {
     const content = fs.readFileSync(filePath, 'utf8');
     const document = new DOMParser().parseFromString(content);
 
     const nodes = xpath.select(xpathString, document);
-    for (const node of nodes) {
-        node.firstChild.textContent = replaceString;
+    if (nodes.length === 0) {
+        core.setFailed("No matching xml nodes found");
+    } else {
+        for (const node of nodes) {
+            console.log("Setting xml value at " + getNodePath(node));
+            if (replaceString === null) {
+                node.parentNode.removeChild(node);
+            } else {
+                node.textContent = replaceString;
+            }
+        }
+        fs.writeFileSync(filePath, new XMLSerializer().serializeToString(document));
     }
+}
 
-    fs.writeFileSync(filePath, new XMLSerializer().serializeToString(document));
-} catch (error) {
-    console.log(error)
-    core.setFailed(error.message);
+function getNodePath(node) {
+    let parentNode = node.parentNode;
+    if (parentNode == null) {
+        return node.nodeName;
+    }
+    return (getNodePath(parentNode)) + "/" + node.nodeName;
 }
