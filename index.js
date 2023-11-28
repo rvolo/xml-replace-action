9cb7c33ced7030e73f1b70b94293fe8ddb221779
Patrik Sjöberg <patrik.sjoberg@sokigo.com>
2023-11-28 13:16:40 +0100

    Added Support for node replacement

--- index.js
+++ index.js
@@ -4,19 +4,34 @@ const DOMParser = require("xmldom").DOMParser;
 const XMLSerializer = require("xmldom").XMLSerializer;
 const fs = require("fs");
 
+const XML_NODETYPE = {
+  ELEMENT_NODE: 1,
+  ATTRIBUTE_NODE: 2,
+  TEXT_NODE: 3,
+  CDATA_SECTION_NODE: 4,
+  ENTITY_REFERENCE_NODE: 5,
+  ENTITY_NODE: 6,
+  PROCESSING_INSTRUCTION_NODE: 7,
+  COMMENT_NODE: 8,
+  DOCUMENT_NODE: 9,
+  DOCUMENT_TYPE_NODE: 10,
+  DOCUMENT_FRAGMENT_NODE: 11,
+  NOTATION_NODE: 12,
+};
+
 try {
   let filePath = core.getInput("filepath", { required: true });
   let xpathString = core.getInput("xpath", { required: true });
   let replaceString = core.getInput("replace");
-  let findString = core.getInput("substring");
+  let newValueString = core.getInput("newvalue");
 
-  writeFile(filePath, xpathString, replaceString, findString);
+  writeFile(filePath, xpathString, replaceString, newValueString);
 } catch (error) {
   console.log(error);
   core.setFailed(error.message);
 }
 
-function writeFile(filePath, xpathString, replaceString, findString) {
+function writeFile(filePath, xpathString, replaceString, newValueString) {
   const content = fs.readFileSync(filePath, "utf8");
   const document = new DOMParser().parseFromString(content);
 
@@ -26,15 +41,23 @@ function writeFile(filePath, xpathString, replaceString, findString) {
   } else {
     for (const node of nodes) {
       console.log("Setting xml value at " + getNodePath(node));
+      if (
+        node.nodeType === XML_NODETYPE.ELEMENT_NODE &&
+        replaceString !== null
+      ) {
+        const newNode = new DOMParser().parseFromString(replaceString);
+        node.parentNode.replaceChild(newNode, node);
+      }
+
       if (replaceString === null) {
         node.parentNode.removeChild(node);
       } else {
-        if (findString.length == 0) {
+        if (newValueString.length == 0) {
           node.textContent = replaceString;
         } else {
           node.textContent = node.textContent.replace(
             replaceString,
-            findString
+            newValueString
           );
         }
       }
