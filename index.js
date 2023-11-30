1d7c954dcf0074da9f788f635acb2d58aa9769ba
Patrik Sjöberg <patrik.sjoberg@sokigo.com>
2023-11-30 14:48:19 +0100

    Minor Cleanup

--- index.js
+++ index.js
@@ -22,12 +22,11 @@ const XML_NODETYPE = {
 try {
   let filePath = core.getInput("filepath", { required: true });
   let xpathString = core.getInput("xpath", { required: true });
-  let replaceString = core.getInput("replace");
+  let replaceString = core.getInput("replace", { required: true });
   let newValueString = core.getInput("newvalue");
 
   writeFile(filePath, xpathString, replaceString, newValueString);
 } catch (error) {
-  console.log(error);
   core.setFailed(error.message);
 }
 
@@ -40,7 +39,6 @@ function writeFile(filePath, xpathString, replaceString, newValueString) {
     core.setFailed("No matching xml nodes found");
   } else {
     for (const node of nodes) {
-      console.log("Setting xml value at " + getNodePath(node));
       if (
         node.nodeType === XML_NODETYPE.ELEMENT_NODE &&
         replaceString !== null
@@ -64,12 +62,4 @@ function writeFile(filePath, xpathString, replaceString, newValueString) {
     }
     fs.writeFileSync(filePath, new XMLSerializer().serializeToString(document));
   }
-}
-
-function getNodePath(node) {
-  let parentNode = node.parentNode;
-  if (parentNode == null) {
-    return node.nodeName;
-  }
-  return getNodePath(parentNode) + "/" + node.nodeName;
 }
